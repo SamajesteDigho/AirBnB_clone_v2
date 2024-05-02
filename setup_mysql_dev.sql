5a73d6a3716812e33e351ff2e5d20fb3dd345e06
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-02 10:53:10 +0800

    Exo 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,7 +7,7 @@ use hbnb_dev_db;
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant the given user specific priviledges
-GRANT ALL ON hbnb_dev_db.* TO hbnb_dev;
+GRANT ALL ON hbnb_dev_db.* TO hbnb_dev@localhost;
 
 -- Grant specific priviledge of specific table
-GRANT SELECT ON performance_schema.* TO hbnb_dev;
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
