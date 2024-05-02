c92cd5193720269af03f45a6dd618cc1cd8712a8
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-02 10:43:07 +0800

    Exo 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- Script for preparing the database
+-- Create the database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+use hbnb_dev_db;
+
+-- Create a user and set his password
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant the given user specific priviledges
+GRANT ALL ON hbnb_dev_db.* TO hbnb_dev;
+
+-- Grant specific priviledge of specific table
+GRANT SELECT ON performance_schema TO hbnb_dev;
