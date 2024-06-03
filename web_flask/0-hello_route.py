2efafceb9534917e7ee4586f141c1e266aa6ebc7
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-03 08:26:50 +0800

    Exo 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,9 @@ from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+if __name__ == "__main__":
+    """ Here the initializer """
+    app.run(host="0.0.0.0", port=5000)
 
 
 @app.route("/")
