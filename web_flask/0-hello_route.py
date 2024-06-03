3a43fe93caac731048a69b144166db66547da7ad
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-03 08:28:24 +0800

    Exo 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,14 @@ from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-if __name__ == "__main__":
-    """ Here the initializer """
-    app.run(host="0.0.0.0", port=5000)
 
 
 @app.route("/")
 def home():
     """ Here the slash path. """
     return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    """ Here the initializer """
+    app.run(host="0.0.0.0", port=5000)
