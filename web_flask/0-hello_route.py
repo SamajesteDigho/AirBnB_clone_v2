7760a5d30d0a5d483cf5f4d5740f1deaccf117de
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-03 08:13:30 +0800

    Exo 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def home():
     """ Here the slash path. """
     return "Hello HBNB!"
